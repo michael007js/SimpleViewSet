66328b8f74bafa9afffe85abec7f622471581b2b
michael007js <616425434@qq.com>
2022-09-19 09:23:29 +0800

    新增水波纹扩散

--- app/src/main/java/com/sss/michael/simpleview/utils/Log.java
+++ app/src/main/java/com/sss/michael/simpleview/utils/Log.java
@@ -7,7 +7,7 @@ public class Log {
     public static void log(String tag,Object...objects){
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < objects.length; i++) {
-            stringBuilder.append(" \n").append(objects[i]);
+            stringBuilder.append(stringBuilder.length() == 0?"":" \n").append(objects[i]);
         }
 //        stringBuilder.append(" \n").append("-------------------------------------------------------------------------------------------------------------");
         android.util.Log.e(tag,stringBuilder.toString());
